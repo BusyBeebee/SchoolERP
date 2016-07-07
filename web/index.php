0c5714066ab866e026654cf1225d17d4fb2df196
wtzt <wangzhao0216@foxmail.com>
2016-07-07 13:31:03 +0800

    about mysql

--- web/index.php
+++ web/index.php
@@ -5,7 +5,37 @@
 	<title>Èë¿Ú</title>
 </head>
 <?php
- echo "Hello php!";
+	 	$con = mysql_connect("localhost","root","root");
+	 	if($con)
+	 		echo "mysql connect success!\n";
+	 	else
+	 		echo "mysql connect false!".mysql_errno()."<br/>";
+	 	$sqlstr = "create database mytest";
+	 	if(mysql_query($sqlstr,$con))
+	 	{
+	 		echo "query suc!"."<br/>";
+	 	}
+	 	else
+	 	{
+	 		echo "query false!".mysql_errno()."<br/>";
+	 	}
+	 	mysql_select_db("mytest",$con);
+	 	$sqlstr = "create table testtable
+	 	(
+	 			a int default 0,
+	 			b int default 0,
+	 			c int default 0
+	 	)";
+	 	if(mysql_query($sqlstr,$con))
+	 	{
+	 		echo "query suc!"."<br/>";
+	 	}
+	 	else
+	 	{
+	 		echo "query false!".mysql_errno()."<br/>";
+	 	}
+	 	
+	 	mysql_close($con);
 ?>
 
 </body>
